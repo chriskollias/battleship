5bbc0064eeec46f473529bcb6dd3ece75689d0d8
chriskollias <chriskollias500@gmail.com>
2020-05-05 04:01:22 -0700

    created django project, created basic board in CSS

---
+++ battleship-backend/user_interface/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main_game_view(request):
+    rowCount = 10
+    columnCount = 10
+
+    rows = [x for x in range(0, rowCount)]
+    columns = [x for x in range(0, columnCount + 1)]
+
+
+
+    return render(request, 'user_interface/main_page.html', {'rows': rows, 'columns': columns})
